14458dfc6edb613e27d39c7c631f280014fe0375
Marco Ziliani <marco.ziliani@musement.com>
2021-03-03 13:10:40 +0100

    add example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [cats, setCats] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [isError, setError] = useState(false);
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const response = await fetch(
+  //       "https://api.thecatapi.com/v1/images/search"
+  //     );
+  //     const cats = await response.json();
+  //     setCats(cats);
+  //   }
+  //   fetchData();
+  // }, []);
+
+  useEffect(() => {
+    console.log("useEffect");
+    setLoading(true);
+    setError(false);
+    fetch("https://api.thecatapi.com/v1/images/search")
+      .then((response) => response.json())
+      .then((cats) => {
+        setCats(cats);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+        setError(true);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +42,21 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => setCount(count + 1)}>Counter: {count}</button>
+        {!isLoading ? (
+          cats.map((cat) => (
+            <img
+              src={cat.url}
+              height={cat.height}
+              width={cat.width}
+              alt="cat"
+              key={cat.id}
+            />
+          ))
+        ) : (
+          <h2>Loading...</h2>
+        )}
+        {isError&&<h2>Si è verificato un errore</h2>}
       </header>
     </div>
   );
